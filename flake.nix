f50ea210041061a127e177b2ebdf853bcbb587bd
andystopia <91233633+andystopia@users.noreply.github.com>
2023-11-04 21:48:31 -0400

    Revamped the development environment.
    - Split devshell into a simple and a "nice" one.
    - Remove dependence on flake-utils
    - Remove weird system specific ld version,
       this introduces the need to have zlib
       which is on my system but not recorded.
    - Remove dependence on unused clang
    - Change from the hack of setting DYLD_PATH, to
      actually modifying the ghdl1-llvm binary
      to have the correct llvm path.
    - Ensure the correct LLVM is available at build
      time
    - Add out-of-the-box support for MacOS ARM + x86
    - Still needs Rosetta
    - Use the llvm tools that are available.
    - Simplified the shell-hook as a result.

--- flake.nix
+++ flake.nix
@@ -1,54 +1,98 @@
 {
-  description = "A flake which downloads GHDL v3.0.0 MacOS11-llvm, installs LLVM and sets the path correctly so that ghdl can compile";
+  description = "Download GHDL v3.0.0 MacOS11-llvm, collect deps, fix-up llvm path, and create a devshell";
   inputs = {
     nixpkgs.url = "nixpkgs/nixos-23.05";
   };
 
-  outputs = { self, nixpkgs, flake-utils}:
+  outputs = { self, nixpkgs }:
 	   	let
-      	pkgs = nixpkgs.legacyPackages.x86_64-darwin;
-    	in rec {
-		    packages = {
-		      myghdl = pkgs.stdenv.mkDerivation {
+				supportedSystems = ["x86_64-darwin" "aarch64-darwin"];
+				forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
 
-						name = "ghdl";
-						src = builtins.fetchurl {
-							url = "https://github.com/ghdl/ghdl/releases/download/v3.0.0/ghdl-macos-11-llvm.tgz";
-							sha256 = "sha256-y4xq0+Z2Pnw0bM7hEdr3YUFsfVSsBwT4olYGQfw8LYU=";
-						};
-						phases = ["unpackPhase" "installPhase"];
-						unpackPhase = "mkdir extract && tar -xzvf $src -C extract";
-						installPhase = "ls extract/ && cp -R extract/ $out";
+				
+				# unfortunately, due to a lack of arm builds,
+				# and my inability to create them, this software
+				# needs to be run under Rosetta. So even, though, 
+				# we "support" ARM machines, we don't support ARM builds.
+				pkgsFor = forAllSystems (system: import nixpkgs { system = "x86_64-darwin"; });
+    	in rec {
+
+		    packages = forAllSystems(system:
+					let 
+						pkgs = pkgsFor.${system};
+					in
+					{
+						# we download the ghdl package, and it should work
+						# with just a couple inputs
+			      ghdl-llvm = pkgs.stdenvNoCC.mkDerivation {
+							# we will need to edit the library
+						  # search path for VHDL.
+							nativeBuildInputs = with pkgs; [
+									llvmPackages_15.bintools
+							];
+
+							# we will need to have libllvm
+							# as a dependency
+							buildInputs = with pkgs; [
+									llvmPackages_15.libllvm 
+							];
+							
+							name = "ghdl";
+							src = builtins.fetchurl {
+								url = "https://github.com/ghdl/ghdl/releases/download/v3.0.0/ghdl-macos-11-llvm.tgz";
+								sha256 = "sha256-y4xq0+Z2Pnw0bM7hEdr3YUFsfVSsBwT4olYGQfw8LYU=";
+							};
+							phases = ["unpackPhase" "installPhase"];
+							unpackPhase = "mkdir extract && tar -xzvf $src -C extract";
+							installPhase = ''
+								# first fix the path of the downloaded binary to be what we'd expect
+								llvm-install-name-tool -change /usr/local/opt/llvm/lib/libLLVM.dylib $(llvm-config --libdir --link-shared)/libLLVM.dylib extract/bin/ghdl1-llvm 
+								# and move it to the output where we can execute it
+								cp -R extract/ $out
+							'';
 		     		};
-				};
+				});
 
 
 
 				# This is the same devshell for x86 as well, so just change the arch
-        devShell.aarch64-darwin = pkgs.mkShell {
-          name = "ghdl-devshell";
-          buildInputs = [ 
-						packages.myghdl # use the GHDL that we just unpacked from the website
-						pkgs.llvmPackages_15.libllvm # `ghdl --version` says that it wants llvm 15.0.
-						pkgs.llvmPackages_15.libclang # maybe we need clang too
-						pkgs.gtkwave # we want to be able to view the generated vcd files
-						pkgs.starship # I like a nice bash prompt
-						pkgs.just # A simple command runner
-						(pkgs.python39.withPackages(ps: with ps; [ tkinter ]))
-					];  # Add any additional development tools or dependencies here
-					shellHook = ''
-							# make the prompt pretty :)
-							eval "$(starship init bash)"
 
-							# for ~some~ reason a broken clang 11 seems to get installed
-							# too, we don't want this to lead the path, so let's just 
-							# stick with the same versino of clang as llvm.
-							export PATH="${pkgs.llvmPackages_15.libclang}/bin:$PATH"
-							# set the path for clang
-							export LIBCLANG_PATH="${pkgs.llvmPackages_15.libclang}/lib"
-							# make sure ghdl can find llvm
-							export DYLD_LIBRARY_PATH="$(llvm-config --libdir --link-shared)";
-					'';
-        };
-		 };
+				devShells = forAllSystems(system: 
+					let 
+            pkgs = pkgsFor.${system};
+            # add our base set of dependencies here
+            basePkgs = with pkgs; [
+									gtkwave # we want to be able to view the generated vcd files
+									zlib # for some reason -lz is passed to the linker by ghdl
+									(python39.withPackages(ps: with ps; [ tkinter ])) # python wants tkinter for vcd_movie
+            ] ++ # and of course, we need to include our ghdl binary
+						[packages.${system}.ghdl-llvm];
+
+
+						# ghdl expects to have clang 15 at runtime
+						stdenv = pkgs.llvmPackages_15.stdenv;
+       
+					in {
+						# create a bare bones devshell
+						default = stdenv.mkDerivation {
+							name = "ghdl-devshell";
+		          buildInputs = basePkgs;
+  	        };
+						# create a nicer dev environment shell
+						nice = stdenv.mkDerivation { 
+							name = "ghdl-devshell-fancy";
+							buildInputs = basePkgs ++ (with pkgs; [
+								starship # I like a nice bash prompt
+								just     # A simple command runner
+							]);
+
+							
+							shellHook = ''
+									# make the prompt pretty :)
+									eval "$(starship init bash)"
+							'';
+		 				};
+  				}
+				);
+     };
 }
